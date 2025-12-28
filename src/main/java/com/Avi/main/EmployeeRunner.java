a4e5a21c13d2842d5d9216e4d33167ad32d1c0ca
AvyaGupta-14 <avyagupta7@gmail.com>
2025-12-29 03:39:10 +0530

    Initial commit

---
+++ src/main/java/com/Avi/main/EmployeeRunner.java
@@ -0,0 +1,25 @@
+package com.Avi.main;
+
+import com.Avi.dao.EmployeeDaoImpl;
+
+import java.sql.SQLException;
+
+import com.Avi.dao.EmployeeDao;
+import com.Avi.entity.Employee;
+
+public class EmployeeRunner {
+
+	public static void main(String[] args) throws SQLException {
+
+//		Persistent Object
+		EmployeeDao edao = new EmployeeDaoImpl();
+		Employee emp = new Employee(4, "Sunny", "Male", 67000);
+		
+		//edao.saveEmp(emp);
+		//edao.updateEmp(emp);
+		edao.deleteEmpById(12);
+		System.out.println("Query executed...............");
+
+		
+	}
+}
